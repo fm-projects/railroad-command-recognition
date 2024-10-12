4f4a5a21a16f47bb067771408e612ccae9c4a3b6
Artem Zaborshikov <31626137+zaborshikov@users.noreply.github.com>
2024-10-13 02:43:56 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -99,6 +99,16 @@ stemmed_labels = [{stemmer.stem(word) for word in label}
 
 
 def find_nearest_label(sent: str, stemmed_labels: list[str]) -> tp.Tuple[int, str]:
+    """
+    Находит ближайшую метку для данного предложения.
+
+    Аргументы:
+    sent (str): Входное предложение.
+    stemmed_labels (list[str]): Список стеммированных меток.
+
+    Возвращает:
+    Tuple[int, str]: Кортеж, содержащий индекс метки с максимальным пересечением и саму метку.
+    """
     processed_sent = {stemmer.stem(word) for word in sent.split()}
     intersects = [len(processed_sent.intersection(label)) for label in stemmed_labels]
     max_common_id = np.argmax(intersects)
@@ -106,6 +116,15 @@ def find_nearest_label(sent: str, stemmed_labels: list[str]) -> tp.Tuple[int, st
 
 
 def extract_number(label: list[str]) -> tp.Tuple[int, str]:
+    """
+    Извлекает число из строки с предсказанным текстом.
+
+    Аргументы:
+    label (list[str]): Строка с предсказаниями, превращенная в список.
+
+    Возвращает:
+    tp.Tuple[int, str]: Кортеж, содержащий сумму чисел и строку с объединенными числовыми метками.
+    """
     res_list = []
     res_num = 0
     for num_str in list(number_dict.keys())[::-1]:
@@ -116,6 +135,15 @@ def extract_number(label: list[str]) -> tp.Tuple[int, str]:
 
 
 def get_vagon_form(n: int) -> str:
+    """
+    Возвращает правильную форму слова "вагон" в зависимости от числа.
+
+    Аргументы:
+    n (int): Число вагонов.
+
+    Возвращает:
+    str: Правильная форма слова "вагон" ("вагон", "вагона" или "вагонов").
+    """
     if n % 10 == 1 and n % 100 != 11:
         return "вагон"
     elif 2 <= n % 10 <= 4 and not (12 <= n % 100 <= 14):
@@ -125,6 +153,15 @@ def get_vagon_form(n: int) -> str:
 
 
 def postprocess_vagon(str_label: str) -> str:
+    """
+    Обрабатывает строку метки вагона, извлекая номер и форму вагона, и заменяет соответствующие части строки.
+
+    Аргументы:
+    str_label (str): Исходная строка метки вагона.
+
+    Возвращает:
+    str: Обработанная строка метки вагона с заменёнными номером и формой.
+    """
     label = str_label.split()
     voice_num, voice_str = extract_number(label)
     vagon_form = get_vagon_form(voice_num)
